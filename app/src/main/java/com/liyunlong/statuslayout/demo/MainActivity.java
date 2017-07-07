7521261f67bc625c7d1d5b85c70ccd6c73c90b0a
李云龙 <873825813@qq.com>
2017-07-07 15:06:29 +0800

    添加替换模式和覆盖模式的测试

--- app/src/main/java/com/liyunlong/statuslayout/demo/MainActivity.java
+++ app/src/main/java/com/liyunlong/statuslayout/demo/MainActivity.java
@@ -1,16 +1,20 @@
 package com.liyunlong.statuslayout.demo;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.liyunlong.statuslayout.OnRetryListener;
+import com.liyunlong.statuslayout.OnRetryActionListener;
 import com.liyunlong.statuslayout.OnStatusLayoutChangedListener;
 import com.liyunlong.statuslayout.StatusLayoutManager;
 
@@ -21,7 +25,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public static final String[] actions = {"有数据", "无数据", "加载中", "加载失败", "网络异常", "网络不佳"};
     private TextView tvContent;
     private Toast toast;
+    private int currentPosition;
     private StatusLayoutManager statusLayoutManager;
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +39,25 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Arrays.asList(actions));
         gvMenu.setAdapter(adapter);
         gvMenu.setOnItemClickListener(this);
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.rg_mode);
+        RadioButton rbReplace = (RadioButton) findViewById(R.id.rb_replace);
+        rbReplace.setChecked(true);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                statusLayoutManager.restoreLayout(); // 恢复显示数据的View
+                if (checkedId == R.id.rb_replace) {
+                    statusLayoutManager = statusLayoutManager.newBuilder()
+                            .setUseOverlapStatusLayoutHelper(false)
+                            .build();
+                } else if (checkedId == R.id.rb_overlap) {
+                    statusLayoutManager = statusLayoutManager.newBuilder()
+                            .setUseOverlapStatusLayoutHelper(true)
+                            .build();
+                }
+                updateStatusLayout(currentPosition); // 还原当前状态
+            }
+        });
 
     }
 
@@ -44,44 +69,36 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 .setLoadingLayout(R.layout.layout_loading)
                 .setNetWorkErrorLayout(R.layout.layout_no_network)
                 .setNetWorkPoorLayout(R.layout.layout_bad_network)
-                .setRetryViewId(R.id.action)
+                .setRetryViewId(R.id.action_retry)
                 .setUseOverlapStatusLayoutHelper(false)
                 .setOnStatusLayoutChangedListener(new OnStatusLayoutChangedListener() {
                     @Override
                     public void onStatusLayoutChanged(View currentLayout) {
 
                     }
-                }).setOnRetryListener(new OnRetryListener() {
+                }).setOnRetryActionListener(new OnRetryActionListener() {
                     @Override
-                    public void onRetry(View view) {
+                    public void onRetryAction(View view) {
                         showToast("开始加载...");
                         statusLayoutManager.showLoadingLayout();
-
-                        new Thread(new Runnable() {
+                        mHandler.postDelayed(new Runnable() {
                             @Override
                             public void run() {
-                                try {
-                                    Thread.sleep(1000);
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        showToast("加载成功");
-                                        statusLayoutManager.restoreLayout();
-                                    }
-                                });
+                                showToast("加载成功");
+                                statusLayoutManager.restoreLayout();
                             }
-                        }).start();
-
+                        }, 1500);
                     }
                 }).build();
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        updateStatusLayout(position);
+    }
+
+    private void updateStatusLayout(int position) {
+        this.currentPosition = position;
         switch (position) {
             case 0:
                 statusLayoutManager.restoreLayout();
